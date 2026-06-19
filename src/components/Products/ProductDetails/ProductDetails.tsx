e1ca91054313643dca498363b88c467b480e7db6
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-06-19 18:50:15 +0530

    feat:add useQuery in Product Details Page

--- src/components/Products/ProductDetails/ProductDetails.tsx
+++ src/components/Products/ProductDetails/ProductDetails.tsx
@@ -1,35 +1,27 @@
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
 import { ProductDescription } from './ProductDescription';
 import { ProductImages } from './ProductImages';
-import { useEffect, useState } from 'react';
 
 import { ProductSkeleton } from '../Skeleton/ProductSkeleton';
-import type { Product } from '../ProductList/ProductList';
 import { getProduct } from '../services/productService';
 
 export const ProductDetails = () => {
   const { productId } = useParams();
 
-  const [product, setProduct] = useState<Product>();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchProduct() {
-      try {
-        if (productId) {
-          const data: Product = await getProduct(productId);
-          setProduct(data);
-        }
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
+  const { data: product, isLoading } = useQuery({
+    queryKey: ['product', productId],
+    queryFn: () => {
+      if (!productId) {
+        throw new Error('Product ID is required');
       }
-    }
-    fetchProduct();
-  }, [productId]);
+      return getProduct(productId);
+    },
+    enabled: !!productId,
+  });
 
-  if (loading) {
+  if (isLoading || !product) {
     return <ProductSkeleton />;
   }
 
@@ -37,10 +29,10 @@ export const ProductDetails = () => {
     <main className="bg-tertiary px-4 pt-24 pb-16 md:px-8 lg:px-16">
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-10 lg:grid-cols-2 lg:gap-16">
         {/* LEFT SECTION */}
-        <ProductImages images={product!.images} />
+        <ProductImages images={product.images} />
 
         {/* RIGHT SECTION */}
-        <ProductDescription product={product!} />
+        <ProductDescription product={product} />
       </div>
     </main>
   );
