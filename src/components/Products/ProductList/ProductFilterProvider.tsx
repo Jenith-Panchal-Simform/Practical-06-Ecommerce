bdddfa6b974cc91c4da77a0ed0046246776fa16e
Jenith-Panchal-Simform <jenith.panchal@simformsolutions.com>
2026-06-19 12:47:44 +0530

    feat:add searchParams in context

--- src/components/Products/ProductList/ProductFilterProvider.tsx
+++ src/components/Products/ProductList/ProductFilterProvider.tsx
@@ -1,5 +1,6 @@
 import { useState, type ReactNode } from 'react';
 import { ProductFilterContext } from '../hooks/useFilter';
+import { useSearchParams } from 'react-router-dom';
 
 type ProductProviderProps = {
   children: ReactNode;
@@ -7,12 +8,15 @@ type ProductProviderProps = {
 
 export const ProductProvider = ({ children }: ProductProviderProps) => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
 
   return (
     <ProductFilterContext.Provider
       value={{
         searchTerm,
         setSearchTerm,
+        searchParams,
+        setSearchParams,
       }}
     >
       {children}
--- src/components/Products/hooks/useFilter.tsx
+++ src/components/Products/hooks/useFilter.tsx
@@ -1,8 +1,11 @@
 import { createContext, useContext, type Dispatch, type SetStateAction } from 'react';
+import type { SetURLSearchParams } from 'react-router-dom';
 
 type ProductFilterContextType = {
   searchTerm: string;
   setSearchTerm: Dispatch<SetStateAction<string>>;
+  searchParams: URLSearchParams;
+  setSearchParams: SetURLSearchParams;
 };
 
 export const ProductFilterContext = createContext<ProductFilterContextType | undefined>(undefined);
